62108599b1c7df850b57a2cde5df644a74c4a62d
Jimmy <2947244682@qq.com>
2024-05-29 14:31:25 +0800

    非常完美

--- app/src/main/java/com/simplelife/ss/MainActivity.kt
+++ app/src/main/java/com/simplelife/ss/MainActivity.kt
@@ -6,18 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.view.WindowCompat
 import com.simplelife.ss.ui.theme.SecondScreenTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,19 +23,18 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             SecondScreenTheme() {
-                Content(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(0.dp, 50.dp, 0.dp, 0.dp)
-                )
+                Content()
             }
         }
     }
 }
 
 @Composable
-fun Content(modifier: Modifier = Modifier) {
-    Box(modifier = modifier) {
+fun Content() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = MaterialTheme.colorScheme.secondary)
+        .padding(0.dp, 50.dp, 0.dp, 0.dp)) {
         Row(
             modifier = Modifier
                 .height(50.dp)
@@ -68,9 +62,7 @@ fun Content(modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Content(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(12.dp, 50.dp, 12.dp, 0.dp)
-    )
+    SecondScreenTheme() {
+        Content()
+    }
 }
--- app/src/main/java/com/simplelife/ss/ui/theme/Color.kt
+++ app/src/main/java/com/simplelife/ss/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.simplelife.ss.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightBlue = Color(0xFF81D4FA)
+val LightBlueVariant = Color(0xFF4FC3F7)
+val LightBlueDark = Color(0xFF29B6F6)
+val LightBlueLight = Color(0xFFB3E5FC)
+val LightBlueAccent = Color(0xFF80D8FF)
